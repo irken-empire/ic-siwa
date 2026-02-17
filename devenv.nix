82e0aea02004b15df33260da8f12510e7cf59cbd
Operation Impending Doom <doom@irkenempire.tech>
2026-02-18 00:00:39 +1100

    chore: update devenv.lock

--- devenv.nix
+++ devenv.nix
@@ -48,9 +48,9 @@ let
   #packages = with pkgs; [ ];
 
   packagesUnstable = with pkgsUnstable; [
-    tailwindcss_4
+    claude-code-bin
     gemini-cli-bin
-    claude-code
+    tailwindcss_4
   ];
 
   devPackages =
