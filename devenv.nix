51919e083394990d04f03832c3c684e123e55624
Operation Impending Doom <doom@irkenempire.tech>
2026-03-19 17:44:29 +1100

    fix: pre-commit hooks

--- devenv.nix
+++ devenv.nix
@@ -255,11 +255,19 @@ in
 
   git-hooks = {
     excludes = [
+      "**/candid/*.ts"
+      "*.did"
+      ".dfx/"
       ".direnv/"
       ".dist/"
       ".git/"
       ".vscode/"
+      "^contracts/" # Using custom pre-commit hook.
       "^docs/agents/.*"
+      "dist/"
+      "node_modules/"
+      "target/"
+      ".vscode/"
     ];
     hooks = {
       actionlint.enable = true;
